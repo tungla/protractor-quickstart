35811c16989a05713fcd620123beed22446320dc
Tung Vu <tung.vu@tiki.vn>
2018-06-11 15:08:24 +0700

    initial commit

---
+++ conf.js
@@ -0,0 +1,12 @@
+// conf.js
+exports.config = {
+  framework: 'jasmine',
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  specs: ['spec.js'],
+  capabilities: {
+    browserName: 'chrome',
+    chromeOptions: {
+        args: ['--window-size=1366,768'] // THIS!
+    }
+  }
+}
